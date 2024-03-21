19b1654c8c1bdd46f8440659638792c1f58f5f39
Brain Kim <48711138+iimkbsu@users.noreply.github.com>
2024-03-21 15:31:28 +0900

    Add files via upload

--- update.php
+++ update.php
@@ -4,7 +4,7 @@ session_start();
 
 $sid = $_SESSION['id'];
 
-$conn = mysqli_connect('localhost','root','620328','bulletin');
+$conn = mysqli_connect('localhost', 'root', '620328', 'bulletin');
 $sql = "SELECT * FROM notice WHERE idx='{$_GET['idx']}'";
 $result = mysqli_query($conn, $sql);
 $arr;
@@ -12,20 +12,20 @@ $idx;
 $head;
 $title;
 $description;
+$ufile;
 
-if(isset($result)) {
+if (isset ($result)) {
    $arr = mysqli_fetch_array($result);
-   if($arr['writer'] == $_SESSION['id']) {
+   if ($arr['writer'] == $_SESSION['id']) {
       $idx = $arr['idx'];
       $head = $arr['head'];
       $title = $arr['title'];
       $description = $arr['description'];
+      $ufile = $arr['ufile'];
    }
-}else {
-   echo "글을 불러오는 과정에서 문제가 생겼습니다. 관리자에게 문의하세요.";
+} else {
+   echo "<script>alert('문제가 발생했습니다. 관리자에게 문의하세요.'); location.href='index.html'</script>";
 }
-
-
 
 
 ?>
@@ -33,29 +33,68 @@ if(isset($result)) {
 
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>글 수정</title>
+
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" 
+   integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" 
+   crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 </head>
+
 <body>
-   <form id="updateForm" action="updateProcess.php?idx=<?=$_GET['idx']?>" method="post">
+   <form id="updateForm" action="updateProcess.php?idx=<?= $_GET['idx'] ?>" method="post" enctype="multipart/form-data">
       <fieldset>
          <legend>글 수정</legend>
          <select name="head" form="updateForm">
-         <?php
-            if(($_SESSION['id'] == 'admin' || $_SESSION['id'] == 'ADMIN')) {
+            <?php
+            if (($_SESSION['id'] == 'admin' || $_SESSION['id'] == 'ADMIN')) {
                echo "<option value=\"notice\">공지</option>";
             }
             ?>
-               <option value="normal">일반</option>
-               <option value="question">질문</option>
-               <option value="vote">투표</option>
+            <option value="normal">일반</option>
+            <option value="question">질문</option>
+            <option value="vote">투표</option>
          </select>
-         <input type="text" name="title" value="<?=$title?>">
-         <p><textarea name="description"><?=$description?></textarea></p>
+         <input type="text" name="title" value="<?= $title ?>">
+         <p><textarea name="description" rows="20px" cols="100px"><?= $description ?></textarea></p>
+
+         <input type='file' id='image' name='userfile[]' onchange='setview(event)' onclick='removechild()' multiple>
+         <br>
+         <!-- 이미지 미리보기 -->
+         <div id="image_container"></div>
+         <br>
          <input type="submit">
       </fieldset>
    </form>
+
+   <script>
+
+      // 이미지 제거
+      function removechild() {
+         $('#image_container').empty();
+      }
+
+      function setview(event) {
+         for (var image of event.target.files) {
+            var reader = new FileReader();
+
+            reader.onload = function (event) {
+
+               var img = document.createElement("img");
+               img.setAttribute("src", event.target.result);
+               img.setAttribute("style", "width:300px; height:200px;");
+               document.querySelector("div#image_container").appendChild(img);
+            };
+            console.log(image);
+            reader.readAsDataURL(image);
+         }
+
+      }
+   </script>
 </body>
+
 </html>
--- updateProcess.php
+++ updateProcess.php
@@ -2,6 +2,7 @@
 session_cache_limiter('');
 session_start();
 
+
 $sid = $_SESSION['id'];
 $idx = $_GET['idx'];
 $head = $_POST['head'];
@@ -10,9 +11,35 @@ $description = $_POST['description'];
 
 $conn = mysqli_connect('localhost', 'root', '620328', 'bulletin');
 
-$sql = "UPDATE notice SET head='{$head}', title='{$title}', description='{$description}' WHERE writer='{$sid}' AND idx='{$idx}'";
+
+
+
+$ufile = $_FILES['userfile'];
+
+$fname = $ufile['name'];
+$fpath = $ufile['full_path'];
+$ftype = $ufile['type'];
+$tname = $ufile['tmp_name'];
+$fsize = $ufile['size'];
+
+$updir = "./userupload/file/";
+
+$arrString = null;
+
+$fnames = array();
+for ($i = 0; $i < count($fname); $i++) {
+   move_uploaded_file($tname[$i], $updir . $fname[$i]); //디렉토리 저장
+   array_push($fnames, $fname[$i]); //fnames 배열에 push
+   $arrString = implode(",", $fnames); // fnames 저장된 배열 문자열로
+}
+
+
+
+$conn = mysqli_connect('localhost', 'root', '620328', 'bulletin');
+
+$sql = "UPDATE notice SET head='{$head}', title='{$title}', description='{$description}', ufile='{$arrString}' WHERE writer='{$sid}' AND idx='{$idx}'";
 $result = mysqli_query($conn, $sql);
-if (isset($result)) {
+if (isset ($result)) {
    echo "<script>alert('글 수정이 완료되었습니다');location.href='bulletinBoard_main.html?idx={$_GET['idx']}'</script>";
 
 } else {
